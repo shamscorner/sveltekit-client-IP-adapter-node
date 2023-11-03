e3433549fa4c3971667dce21d7a98fa20ab6a1e0
Shamim Hossain <shossain@secureship.ca>
2023-11-03 20:59:53 +0600

    chore: add cn utils with tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -8,5 +9,9 @@
 	}
 </script>
 
-<h1 class="bg-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class={cn('text-green-200 text-2xl', 'text-green-500 text-3xl', 'px-5 py-6')}>
+	Welcome to SvelteKit
+</h1>
+<p class="px-5">
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
